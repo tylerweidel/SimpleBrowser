329d9200c8ae0ae68d35486be6c27f41b7f99f08
Tyler Weidel <tjw941@gmail.com>
2019-08-05 12:13:03 -0400

    Initial Commit

---
+++ SimpleBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by Weidel, Tyler on 8/5/19.
+//  Copyright © 2019 Tyler Weidel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
