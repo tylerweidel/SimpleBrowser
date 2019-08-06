47f63bd3f14d7de8eed7222043c92fb0d05c620b
Tyler Weidel <tjw941@gmail.com>
2019-08-05 22:41:40 -0400

    Setup SearchResultsTableViewController and add logic to fetch query string and load web page.

--- SimpleBrowser/BrowserViewController.swift
+++ SimpleBrowser/BrowserViewController.swift
@@ -13,27 +13,90 @@ class BrowserViewController: UIViewController {
 
     @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var searchResultsView: UIView!
+    
+    var urlSession: URLSession?
+    var searchResultsTableViewController: SearchResultsTableViewController? {
+        didSet {
+            setupSearchResultsTableViewController()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupSearchField()
-        
-        let urlSession = URLSession(configuration: .default)
-        
-        BingService(urlSession: urlSession).getSearchResults(using: "fire") { (result) in
-            switch result {
-            case .success(let searchResult):
-                print(searchResult)
-            case .failure(let error):
-                print("Failed to fetch results:", error)
-            }
-        }
+        setupURLSession()
     }
 
     private func setupSearchField() {
         searchField.placeholder = "Search"
         searchField.delegate = self
+    }
+    
+    private func setupURLSession() {
+        urlSession = URLSession(configuration: .default)
+    }
+    
+    private func setupSearchResultsTableViewController() {
+        searchResultsTableViewController?.delegate = self
+        hideSearchResults()
+    }
+    
+    private func showSearchError(with error: Error) {
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func fetchQuery(using query: String, then: @escaping ((SearchResult) -> Void)) {
+
+        guard let urlSession = urlSession else { return }
+        BingService(urlSession: urlSession).getSearchResults(using: query) { [weak self] (result) in
+            switch result {
+            case .success(let searchResult):
+                then(searchResult)
+            case .failure(let error):
+                print("Failed to fetch results:", error)
+                self?.showSearchError(with: error)
+            }
+        }
+    }
+    
+    private func showSearchResults() {
+        searchResultsView.isHidden = false
+    }
+    
+    private func hideSearchResults() {
+        searchResultsView.isHidden = true
+//        searchField.resignFirstResponder()
+    }
+    
+    private func loadWebPage(using query: String) {
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "www.bing.com"
+        components.path = "/search"
+        components.queryItems = [
+            URLQueryItem(name: "q", value: query)
+        ]
+        components.percentEncodedQuery = components.percentEncodedQuery?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        guard let url = components.url else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let searchResultsTVC = segue.destination as? SearchResultsTableViewController {
+            searchResultsTableViewController = searchResultsTVC
+        }
     }
 
 }
@@ -42,11 +105,16 @@ extension BrowserViewController: UITextFieldDelegate {
     
     // Show SearchResultsViewController when textFieldDidBeginEditing is called
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        //
+        showSearchResults()
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
+        hideSearchResults()
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        hideSearchResults()
         return true
     }
     
@@ -55,11 +123,30 @@ extension BrowserViewController: UITextFieldDelegate {
         let text = textField.text ?? ""
         guard let stringRange = Range(range, in: text) else { return false }
         let typedText = text.replacingCharacters(in: stringRange, with: string)
+
+        // If user deleted all text, hide search results
+        if typedText == "" {
+            hideSearchResults()
+            return true
+        } else {
+            showSearchResults()
+        }
         
-        // Fire network call off here, remember to cancel previous network call so we arent wasting data
-        print(typedText)
+        fetchQuery(using: typedText) { [weak self] (result) in
+            DispatchQueue.main.async {
+                self?.searchResultsTableViewController?.update(searchResult: result)
+            }
+        }
+
         return true
     }
-    
+}
+
+extension BrowserViewController: SearchResultsDelegate {
+    func touchedSearchResult(searchResult: String) {
+        hideSearchResults()
+        loadWebPage(using: searchResult)
+        searchField.text = searchResult
+    }
 }
 
--- SimpleBrowser/SearchResultsViewController.swift
+++ SimpleBrowser/SearchResultsViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
+protocol SearchResultsDelegate: class {
+    func touchedSearchResult(searchResult: String)
+}
+
+private let SearchResultIdentifier = "SearchResultIdentifier"
+
 class SearchResultsTableViewController: UITableViewController {
 
+    weak var delegate: SearchResultsDelegate?
+    private var searchResult: SearchResult? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    func update(searchResult: SearchResult) {
+        self.searchResult = searchResult
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let searchResult = searchResult else { return 0 }
+        return searchResult.results.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchResultIdentifier, for: indexPath)
+        cell.textLabel?.text = searchResult?.results[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let searchResult = searchResult else { return }
+        delegate?.touchedSearchResult(searchResult: searchResult.results[indexPath.row])
+        // reset searchResult after tapping on a cell to reset next time typing in search
+        self.searchResult = nil
     }
     
 }
